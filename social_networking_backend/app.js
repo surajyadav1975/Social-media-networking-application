face1e424e5594e8267eaa4bf719b04eba365486
surajyadav1975fk <ysuraj1191@gmail.com>
2024-10-19 00:31:03 +0530

    updated cors

--- social_networking_backend/app.js
+++ social_networking_backend/app.js
@@ -20,7 +20,7 @@ app.use(cookieparser());
 // }));
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://localhost:5173', 
     credentials: true,  
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
     allowedHeaders: ['Content-Type', 'Authorization'] 
