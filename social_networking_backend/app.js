fcc5461d7b09336202ed078dfb344f651b786307
Suraj Yadav <ysuraj1191@gmail.com>
2024-10-18 16:41:00 +0530

    Update app.js

--- social_networking_backend/app.js
+++ social_networking_backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://localhost:3000',
     credentials: true, 
 }));
 
